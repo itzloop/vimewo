ed608a8041590f03763b642b742696dbf586935f
hasti karamdel <h.karamdel@pinket.com>
2022-02-01 03:53:59 +0330

    feat(main-page-layout): init files

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,10 @@
-import util from "util"
-import React, { useCallback, useEffect } from "react";
-import videojs from "video.js";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import { API } from "./api/websocket";
-import { ActionType, Controller } from "./player/controller";
-
-function App() {
-	useEffect(() => {
-		let player = videojs("my-player", {}, function onPlayerReady() {
-			videojs.log("hello from the other side");
-
-			this.play();
-
-			this.on("ended", function() {
-				videojs.log("done...")
-			})
-		});
-
-		let controller = new Controller(API, "abcdef")
-		// controller.on(ActionType.PLAY, () => console.log())
-			
-		player.on("play", (args) => controller.play(player.currentTime()))
-		player.on("pause", (args) => controller.pause(player.currentTime()))
-
-		// setInterval(() => console.log(player.currentTime()), 1000)
-
-		try {
-			API.init("ws://localhost:8080/chat", console.log);
-			// setInterval(() => {
-			// 	let data = JSON.stringify({
-			// 		roomid: "abcedf",
-			// 		action: "pause",
-			// 			data: {
-			// 				time: "1:25:34",
-			// 			}
-			// 		})
-			// 	console.log("dataaa", data)
-			// 	API.send(data) 
-			// }, 1000)
-
-			return () => {
-				API.close()
-			}
-		} catch(err) {
-			console.log(err)
-		}
-
-
-	}, []);
-		
-		
+import MainPage from "./components/main-page";
+function App() {		
   return (
     <div className="App">
-			<video
-					id="my-player"
-					className="video-js"
-					preload="auto">
-				<source src="film.mp4" type="video/mp4"></source>
-
-			</video>
+			<MainPage></MainPage>
     </div>
   );
 }
---
+++ src/components/main-page.js
@@ -0,0 +1,65 @@
+import "../style/main-page.css";
+import React, { useCallback, useEffect } from "react";
+import videojs from "video.js";
+import { API } from "../api/websocket";
+import { ActionType, Controller } from "../player/controller";
+
+
+const MainPage = () => {
+    useEffect(() => {
+		let player = videojs("my-player", {}, function onPlayerReady() {
+			videojs.log("hello from the other side");
+
+			this.play();
+
+			this.on("ended", function() {
+				videojs.log("done...")
+			})
+		});
+
+		let controller = new Controller(API, "abcdef")
+		// controller.on(ActionType.PLAY, () => console.log())
+			
+		player.on("play", (args) => controller.play(player.currentTime()))
+		player.on("pause", (args) => controller.pause(player.currentTime()))
+
+		// setInterval(() => console.log(player.currentTime()), 1000)
+
+		try {
+			API.init("ws://localhost:8080/chat", console.log);
+			// setInterval(() => {
+			// 	let data = JSON.stringify({
+			// 		roomid: "abcedf",
+			// 		action: "pause",
+			// 			data: {
+			// 				time: "1:25:34",
+			// 			}
+			// 		})
+			// 	console.log("dataaa", data)
+			// 	API.send(data) 
+			// }, 1000)
+
+			return () => {
+				API.close()
+			}
+		} catch(err) {
+			console.log(err)
+		}
+
+
+	}, []);
+    return (
+        <div className="main-page-container">
+            <p>hiiiiiiiiiiiiiii</p>
+            <video
+					id="my-player"
+					className="video-js"
+					preload="auto">
+				<source src="film.mp4" type="video/mp4"></source>
+
+			</video>
+        </div>
+    )
+}
+
+export default MainPage;
